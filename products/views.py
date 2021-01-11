143868cd9643fa42bc50578be656a9cec8a12b1e
Michaelstanden <michaelstanden@hotmail.com>
2021-01-11 17:40:04 +0000

    added products.html as well as links to access the products app in urls.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import products
 
 # Create your views here.
+
+def all_products(request):
+    """ A view to return the prodcust page and to handle search query results on products searches """
+
+    products = Products.objects.all()
+
+    context = {
+        'products' : products
+    }
+
+    return render(request, 'products/products.html' colntext)
